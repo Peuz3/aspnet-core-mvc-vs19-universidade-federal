626da271b8a5241694344c211662880fb24c5eff
Joao Pedro <jp.ssilva@live.com>
2020-05-07 11:52:42 -0300

    Utilizando a primeira Migration

---
+++ Migrations/20200507144353_Initial.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UniversidadeFederal.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Curso",
+                columns: table => new
+                {
+                    CursoID = table.Column<int>(nullable: false),
+                    Titulo = table.Column<string>(nullable: true),
+                    Creditos = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Curso", x => x.CursoID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Estudante",
+                columns: table => new
+                {
+                    EstudanteID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    SobreNome = table.Column<string>(nullable: true),
+                    Nome = table.Column<string>(nullable: true),
+                    DataMatricula = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Estudante", x => x.EstudanteID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Matricula",
+                columns: table => new
+                {
+                    MatriculaID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CursoID = table.Column<int>(nullable: false),
+                    EstudanteID = table.Column<int>(nullable: false),
+                    Nota = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Matricula", x => x.MatriculaID);
+                    table.ForeignKey(
+                        name: "FK_Matricula_Curso_CursoID",
+                        column: x => x.CursoID,
+                        principalTable: "Curso",
+                        principalColumn: "CursoID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Matricula_Estudante_EstudanteID",
+                        column: x => x.EstudanteID,
+                        principalTable: "Estudante",
+                        principalColumn: "EstudanteID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Matricula_CursoID",
+                table: "Matricula",
+                column: "CursoID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Matricula_EstudanteID",
+                table: "Matricula",
+                column: "EstudanteID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Matricula");
+
+            migrationBuilder.DropTable(
+                name: "Curso");
+
+            migrationBuilder.DropTable(
+                name: "Estudante");
+        }
+    }
+}
